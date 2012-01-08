747b1a90e844a52fa39fb9ba0b58c39c0c6c533b
Nathan Hwang <beta.entity.k@gmail.com>
2012-01-08 01:55:54 -0800

    Added oauth code from pensievr

--- controller.py
+++ controller.py
@@ -69,7 +69,87 @@ class Index(webapp.RequestHandler):
     def post(self):
         self.get()
 
+# # get the request token (temporary) and redirect to the authorization page
+# class OAuth(webapp.RequestHandler):
+#     def get(self):
+#         session = get_current_session()
+#         # since we're logging in again, kill the current session
+#         if session.is_active():
+#             session.terminate()
 
+#         # get request token
+#         consumer = oauth.Consumer(API_KEY, API_SECRET)
+#         client = oauth.Client(consumer)
+
+#         callback_uri = "https://%s.appspot.com/callback" % get_application_id()
+#         callback_uri = urllib.quote(callback_uri)
+#         request_token_url = TEMP_CRED_URI + "?oauth_callback=%s" % callback_uri
+#         resp, content = client.request(request_token_url, "GET")
+#         if resp['status'] != '200':
+#             raise Exception("Invalid response %s." % resp['status'])
+
+#         # deprecated in 2.6+, use urlparse instead
+#         request_token = dict(cgi.parse_qsl(content))
+
+#         oauth_token = request_token['oauth_token']
+#         oauth_token_secret = request_token['oauth_token_secret']
+#         # set these in session for the callback
+#         session['oauth_token'] = oauth_token
+#         session['oauth_token_secret'] = oauth_token_secret
+#         session['done'] = False
+
+#         # redirect to the auth page
+#         self.redirect(OWNER_AUTH_URI + "?oauth_token=%s" % oauth_token)
+#     def post(self):
+#         self.get()
+
+# # callback endpoint of the OAuth process
+# class OAuthCallback(webapp.RequestHandler):
+#     def get(self):
+#         session = get_current_session()
+#         # make sure the session is active
+#         if not(session.is_active()):
+#             raise Exception("Session is not active")
+
+#         # get the oauth_verifier passed in
+#         oauth_verifier = self.request.get("oauth_verifier", None)
+#         if not(oauth_verifier):
+#             raise Exception("No verifier passed in")
+
+#         # retrieve from session
+#         oauth_token = session['oauth_token']
+#         oauth_token_secret= session['oauth_token_secret']
+
+#         # create the client
+#         token = oauth.Token(oauth_token, oauth_token_secret)
+#         token.set_verifier(oauth_verifier)
+#         consumer = oauth.Consumer(API_KEY, API_SECRET)
+#         client = oauth.Client(consumer, token)
+
+#         token_req = TOKEN_REQUEST_URI
+#         resp, content = client.request(token_req + "?", "GET")
+#         # deprecated in 2.6+, use urlparse instead
+#         access_token = dict(cgi.parse_qsl(content))
+
+#         log = logging.getLogger(__name__)
+#         log.info(access_token)
+
+#         oauth_token = access_token['oauth_token']
+#         user_id = access_token['edam_userId']
+#         shard = access_token["edam_shard"]
+
+#         # store the oauth_token in the session - only temporary data
+#         session['oauth_token'] = oauth_token
+#         session['shard'] = shard
+#         session['done'] = True
+#         session['user'] = user_id
+
+#         user = EvernoteUser.get_or_insert(user_id)
+#         if not(user.user_id):
+#             user.user_id = user_id
+#             user.put()
+
+#         self.redirect("/")
 
 application = webapp.WSGIApplication(
     [('/', Index),
